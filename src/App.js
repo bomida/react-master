80909f822607ae4be796aa0197d27d5d0adffb3d
BomiDo <bm.do.bomhada@gmail.com>
2022-03-28 16:33:43 +0900

    파일 이름 변경

--- src/App.js
+++ src/App.js
@@ -15,3 +15,9 @@ function App() {
 }
 
 export default App;
+
+// 📘 TypeScript
+
+// 타입스크립트는 strongly-typed 언어이다.
+// strongly-typed 란, 프로그래밍 언어가 작동하기 전에 type을 확인하는 것이다.
+// `npx create-react-app {project-name} --template typescript`
